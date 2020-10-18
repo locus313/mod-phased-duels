ed7029c23c26876e285af9caa1bc252a4c5e0be3
locus313 <pat@lo5t.com>
2020-10-17 20:39:21 -0700

    change SpellAuras.h to SpellScript.h

--- src/mod_phased_duels.cpp
+++ src/mod_phased_duels.cpp
@@ -17,7 +17,7 @@ http://emudevs.com/showthread.php/2282-phase-out-dueling-error?p=15483&viewfull=
 #include "Player.h"
 #include "Chat.h"
 #include "GridNotifiers.h"
-#include "SpellAuras.h"
+#include "SpellScript.h"
 
 class PhasedDueling : public PlayerScript
 {
